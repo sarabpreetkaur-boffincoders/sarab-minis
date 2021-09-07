b9c2ccce3b8f82b2cabdc5cff3ebcb6c208dae12
DESKTOP-MDLIEPB\Boffin Coders - 05 <sarabpreet.kaur@boffincoders.com>
2021-09-07 12:18:44 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,6 @@
+function mathTricks(max,min) {
+     if(typeof(max) !== 'number' && typeof(min) !== 'number') {    min = 0;  max = 1;
+    }
+  return (Math.random() * (max-min)) + min;
+  }
+  module.exports = mathTricks;
